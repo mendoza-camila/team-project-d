43cb2a723c5bbe694a2e7e87697499af5dbc14e0
Camila Mendoza <camilamendoza@MacBook-Pro-de-Camila.local>
2025-04-17 19:31:15 -0500

    changes?

---
+++ Tests/test_cl.py
@@ -0,0 +1,16 @@
+import unittest
+import sys
+
+from io import StringIO
+
+from ProductionCode import cl
+
+##def get_activity_data(dataset)
+
+#def get_activity_from_category(activity_category)
+    ##def display_list_of_activities()
+
+class TestCommandLine(unittest.TestCase):
+    def test_get_activity_data(self):
+        '''Test for correct activity data retrieval'''
+        
